9221f876cc27704e54199d1c3ad6a31dc682ab10
fish895623 <dan990429@gmail.com>
2022-06-07 12:26:06 +0000

    First Commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :trans_patch do
+  name 'Trans Patch plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.2'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
